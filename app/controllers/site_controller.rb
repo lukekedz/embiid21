a52a822967bf7076fc777a79fdbc6675b4f426b3
LukeKedz <lukekedziora@gmail.com>
2017-03-20 16:20:16 -0400

    init commit

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,6 @@
+class SiteController < ApplicationController
+
+    def splash
+    end
+
+end
---
+++ config/application.rb
@@ -0,0 +1,19 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Embiid21
+  class Application < Rails::Application
+    config.serve_static_assets = true
+
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = :local
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
