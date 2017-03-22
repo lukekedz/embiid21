fb30879ad6365e424bbcfa8077cd014df3ea42ac
LukeKedz <lukekedziora@gmail.com>
2017-03-22 08:41:08 -0400

    upd migration for statistics; displaying last game

--- db/migrate/20170320202320_create_statistics.rb
+++ db/migrate/20170320202320_create_statistics.rb
@@ -1,21 +1,21 @@
 class CreateStatistics < ActiveRecord::Migration
   def change
     create_table :statistics do |t|
-        t.string :game_date,   null: false
-        t.string :opp,         null: false
-        t.string :score,       null: false
-        t.string :min,         null: false
-        t.string :fgm_fga,     null: false
-        t.string :fg_prct,     null: false
-        t.string :three_pm_pa, null: false
-        t.string :three_prct,  null: false
-        t.string :ftm_fta,     null: false
-        t.string :ft_prct,     null: false
-        t.string :reb,         null: false
-        t.string :ast,         null: false
-        t.string :blk,         null: false
-        t.string :stl,         null: false
-        t.string :pts,         null: false
+        t.string :game_date,  null: false
+        t.string :opp,        null: false
+        t.string :score,      null: false
+        t.string :min,        null: false
+        t.string :fgm_fga,    null: false
+        t.string :fg_prct,    null: false
+        t.string :three_m_a,  null: false
+        t.string :three_prct, null: false
+        t.string :ftm_fta,    null: false
+        t.string :ft_prct,    null: false
+        t.string :reb,        null: false
+        t.string :ast,        null: false
+        t.string :blk,        null: false
+        t.string :stl,        null: false
+        t.string :pts,        null: false
 
         t.timestamps null: false
     end
