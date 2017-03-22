d2a525afe97b1c612fb8dc78dd2b0f030b94c10e
Luke Kedz <luke.kedziora@jefferson.edu>
2017-03-22 08:41:08 -0400

    upd migration for statistics; displaying last game

--- db/schema.rb
+++ db/schema.rb
@@ -17,23 +17,23 @@ ActiveRecord::Schema.define(version: 20170320202320) do
   enable_extension "plpgsql"
 
   create_table "statistics", force: :cascade do |t|
-    t.string   "game_date",   null: false
-    t.string   "opp",         null: false
-    t.string   "score",       null: false
-    t.string   "min",         null: false
-    t.string   "fgm_fga",     null: false
-    t.string   "fg_prct",     null: false
-    t.string   "three_pm_pa", null: false
-    t.string   "three_prct",  null: false
-    t.string   "ftm_fta",     null: false
-    t.string   "ft_prct",     null: false
-    t.string   "reb",         null: false
-    t.string   "ast",         null: false
-    t.string   "blk",         null: false
-    t.string   "stl",         null: false
-    t.string   "pts",         null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "game_date",  null: false
+    t.string   "opp",        null: false
+    t.string   "score",      null: false
+    t.string   "min",        null: false
+    t.string   "fgm_fga",    null: false
+    t.string   "fg_prct",    null: false
+    t.string   "three_m_a",  null: false
+    t.string   "three_prct", null: false
+    t.string   "ftm_fta",    null: false
+    t.string   "ft_prct",    null: false
+    t.string   "reb",        null: false
+    t.string   "ast",        null: false
+    t.string   "blk",        null: false
+    t.string   "stl",        null: false
+    t.string   "pts",        null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
