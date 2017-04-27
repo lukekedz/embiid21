07e73f2e2e5fea8c68e9477c5660f4d1b4ecc63c
LukeKedz <lukekedziora@gmail.com>
2017-04-27 14:24:16 -0400

    styled for Bootstrap; basic fix in place for carousel

--- db/schema.rb
+++ db/schema.rb
@@ -16,41 +16,6 @@ ActiveRecord::Schema.define(version: 20170320202320) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "games", force: :cascade do |t|
-    t.integer  "week_id",    null: false
-    t.datetime "date"
-    t.string   "away",       null: false
-    t.string   "home",       null: false
-    t.float    "spread"
-    t.string   "location"
-    t.string   "start_time"
-    t.boolean  "tiebreaker", null: false
-    t.string   "winner"
-    t.integer  "away_pts"
-    t.integer  "home_pts"
-    t.integer  "total_pts"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "picks", force: :cascade do |t|
-    t.integer  "user_id",    null: false
-    t.integer  "game_id",    null: false
-    t.string   "pick"
-    t.string   "away_home"
-    t.integer  "tbreak_pts"
-    t.boolean  "correct"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "standings", force: :cascade do |t|
-    t.integer  "user_id",    null: false
-    t.integer  "wins"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "statistics", force: :cascade do |t|
     t.string   "game_date",  null: false
     t.string   "opp",        null: false
@@ -67,40 +32,6 @@ ActiveRecord::Schema.define(version: 20170320202320) do
     t.string   "blk",        null: false
     t.string   "stl",        null: false
     t.string   "pts",        null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "teams", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "username"
-    t.boolean  "admin"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "weeks", force: :cascade do |t|
-    t.integer  "week",       null: false
-    t.boolean  "locked"
-    t.boolean  "finalized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
