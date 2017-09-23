fd02cc9886f578e839a7b9e99a78fd83490723dc
Luke Kedz <lukekedziora@gmail.com>
2017-09-23 13:00:20 -0400

    adj for mobile layout

--- app/assets/javascripts/refreshTwitter.js
+++ app/assets/javascripts/refreshTwitter.js
@@ -1,23 +1,21 @@
 $(document).ready(function() {
 
-  var tweetsOnJoelHiddenLi = $(".tweets-on-joel");
-  var tweetsOnJoel         = [];
-
-  for (var i = 0; i < tweetsOnJoelHiddenLi.length; i++) {
-    tweetsOnJoel.push(tweetsOnJoelHiddenLi[i].innerHTML)
-  }
-
   setInterval(function(){
+    var tweetsOnJoelHiddenLi = $(".tweets-on-joel");
+    var tweetsOnJoel         = [];
 
-    // $.ajax({
-    //   type: "GET",
-    //   url: "/site/refresh_twitter",
-    //   data: { tweets_on_joel: tweetsOnJoel },
-    //   success: function(data){
-    //     $("#twitter-on-joel").empty();
-    //     $("#twitter-on-joel").html(data);
-    //   }
-    // });
+    for (var i = 0; i < tweetsOnJoelHiddenLi.length; i++) {
+      tweetsOnJoel.push(tweetsOnJoelHiddenLi[i].innerHTML)
+    }
 
+    $.ajax({
+      type: "GET",
+      url: "/site/refresh_twitter",
+      data: { tweets_on_joel: tweetsOnJoel },
+      success: function(data){
+        $("#twitter-on-joel").empty();
+        $("#twitter-on-joel").html(data);
+      }
+    });
   }, 60000);
 });
