41afabc011e5dbcf3c24fa86a785acb8b1ea4e40
Luke Kedz <lukekedziora@gmail.com>
2017-10-03 18:07:41 -0400

    further developing view

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -53,15 +53,15 @@ class SiteController < ApplicationController
   end
 
   def joel_on_twitter
-  	twitter_initialize().user_timeline('joelembiid', count: 4)
+  	twitter_initialize().user_timeline('joelembiid', count: 20)
   end
 
   def twitter_on_joel
-    twitter_initialize().search('to:joelembiid', result_type: 'recent').take(4)    
+    twitter_initialize().search('to:joelembiid', result_type: 'recent').take(5)    
   end
 
   def refresh_twitter
-    @tweets_joel = twitter_initialize().search('to:joelembiid', result_type: 'recent').take(4)
+    @tweets_joel = twitter_initialize().search('to:joelembiid', result_type: 'recent').take(5)
 
     # @tweets_joel.each_with_index do |t, index|
     #   puts 'DISPLAYED TWEET ID: ' + params[:tweets_on_joel][index]
